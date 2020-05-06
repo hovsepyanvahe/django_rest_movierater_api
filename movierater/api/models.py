901c4948f71ac175b82db8ddc75c6895e7b6551e
hovsepyanvahe <vahikhovsepyan95@gmail.com>
2020-05-07 00:53:00 +0400

    User authentication is created with tokens. Movies and ratings apis are functional now

--- movierater/api/models.py
+++ movierater/api/models.py
@@ -7,6 +7,24 @@ class Movie(models.Model):
     name = models.CharField(max_length=50)
     description = models.TextField(max_length=1500)
 
+    def num_of_ratings(self):
+        ratings = Rating.objects.filter(movie=self)
+        return len(ratings)
+
+    def avg_rating(self):
+        sum = 0
+        ratings = Rating.objects.filter(movie=self)
+        for rating in ratings:
+            sum += rating.stars
+
+        if len(ratings) > 0:
+            return sum // len(ratings)
+        else:
+            return 0
+
+    def __str__(self):
+        return self.name
+
 
 class Rating(models.Model):
     movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
--- movierater/api/urls.py
+++ movierater/api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, include
 from rest_framework import routers
-from .views import MovieViewSet, RatingViewSet
+from .views import MovieViewSet, RatingViewSet, UserViewSet
 
 router = routers.DefaultRouter()
+router.register('users', UserViewSet)
 router.register('movies', MovieViewSet)
 router.register('ratings', RatingViewSet)
 
